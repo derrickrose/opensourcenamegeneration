979d8a5853a22e753df7d1a3b9102ec65e434314
frelin.ampilahy <randofrils@gmail.com>
2019-07-31 17:20:06 -0500

    first push

---
+++ src/main/java/com/g2llc/opensourcenamegeneration/OpensourcenamegenerationApplication.java
@@ -0,0 +1,13 @@
+package com.g2llc.opensourcenamegeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpensourcenamegenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpensourcenamegenerationApplication.class, args);
+	}
+
+}
